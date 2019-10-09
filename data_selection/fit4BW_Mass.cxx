8eda98231f9cb859927aaa68e34c68d4ff6728a6
shaowei <wsw20170224@gmail.com>
2019-10-09 18:23:23 +0800

    update

--- data_selection/fit4BW_Mass.cxx
+++ data_selection/fit4BW_Mass.cxx
@@ -66,10 +66,15 @@ void fit4BW_Mass()
     RooRealVar M4440("M4440", "M4440", 4440.2);
     RooRealVar M4457("M4457", "M4457", 4456.6);
     RooRealVar Mx("Mx", "Mx", 4394.7);
-
+    /*
     RooRealVar x1("x1", "para1", 0.28, -RooNumber::infinity(), RooNumber::infinity());
 	RooRealVar x2("x2", "para2", -0.04, -RooNumber::infinity(), RooNumber::infinity());
 	RooRealVar x3("x3", "para3", 0.0005, -RooNumber::infinity(), RooNumber::infinity());
+    */
+    RooRealVar x1("x1", "para1", 0.28);
+	RooRealVar x2("x2", "para2", -0.04);
+	RooRealVar x3("x3", "para3", 0.0005);
+
 
     RooRealVar gamma4312("gamma4312", "gamma4312", 5.3);
     RooRealVar gamma4440("gamma4440", "gamma4440", 25.2);
