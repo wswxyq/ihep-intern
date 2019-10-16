cc41fd73729722fc4166805b3e25024b15c8e12c
shaowei <wsw20170224@gmail.com>
2019-10-16 16:01:14 +0800

    update

--- data_selection/fit4BW_Mass_background_noloki.cxx
+++ data_selection/fit4BW_Mass_background_noloki.cxx
@@ -37,38 +37,29 @@ void fit4BW_Mass_background_noloki()
 
 	RooRealVar x("B_DTF_M", "B_DTF_M", 4150, 4600, "MeV");
 
-
-    RooRealVar M4312("M4312", "M4312", 4312.0);
-    RooRealVar M4440("M4440", "M4440", 4440.2);
-    RooRealVar M4457("M4457", "M4457", 4456.6);
-    RooRealVar Mx("Mx", "Mx", 4394.7);
-
-    RooRealVar x1("x1", "para1", 3.17395e+01, -1.e6, 1.e6);
-	RooRealVar x2("x2", "para2", 1.21695e-02, -1.e6, 1.e6);
-	RooRealVar x3("x3", "para3", -4.02805e-06, -1.e6, 1.e6);
-	RooRealVar x4("x4", "para4", 1.e-09, -1.e6, 1.e6);
-	RooRealVar x5("x5", "para5", 1.e-012, -1.e6, 1.e6);
-	RooRealVar x6("x6", "para6", 1.e-015, -1.e6, 1.e6);
-
-    RooRealVar sigma("sigma", "sigma", 200, 0, 1000);
-    RooRealVar mean("mean", "mean", 4200, 4150, 4300);
-
-    RooRealVar gauss_frac("gauss_frac", "gauss_frac", 0.2, 0., 1.);
+    RooRealVar x1("x1", "para1", 3.1740e+01, 1.e+01, 5.e+01);
+	RooRealVar x2("x2", "para2", 1.2170e-02, -1.e0, 1.e0);
+	RooRealVar x3("x3", "para3", -4.02805e-06, -1.e-01, 1.e-01);
+	RooRealVar x4("x4", "para4", 1.1642e-09, -1.e-6, 1.e-6);
 
 
-    RooPolynomial poly("poly", "poly", x, RooArgList(x1, x2, x3, x4, x5));
+    RooRealVar sigma("sigma", "sigma", 200, 0, 400);
+    RooRealVar mean("mean", "mean", 4.2000e+03, 4150, 4250);
+
+    RooRealVar gauss_frac("gauss_frac", "gauss_frac", 7.8629e-01, 0., 1.);
+
+
+    RooPolynomial poly("poly", "poly", x, RooArgList(x1, x2, x3, x4));
     RooGaussian gauss("gauss", "gauss", x, mean, sigma);
 
     RooAddPdf background("background", "background", RooArgList(gauss, poly), RooArgList(gauss_frac));
-
-
 
     RooDataSet *ds=new RooDataSet("ds", "ds", RooArgSet(x, B_BDT, B_LOKI_FDS), Import(*chain), 
 		                Cut(""));
     RooDataHist *dh=ds->binnedClone("dh", "dh");
                     
 
-    auto result= background.fitTo(*dh, RooFit::NumCPU(64), RooFit::Save(kTRUE), RooFit::Minos(kTRUE));
+    auto result= background.fitTo(*dh, RooFit::NumCPU(60), RooFit::Save(kTRUE), RooFit::Minos(kTRUE));
 
 	RooPlot* xframe = x.frame(Title("Event p.d.f.")) ;
 
